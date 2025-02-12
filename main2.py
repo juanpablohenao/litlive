19204d592e23dc70fd3b24a84f187885a903601b
juan pablo <juanpablohenao@casamaestra.net>
2025-02-12 14:48:22 -0500

    8000

--- main2.py
+++ main2.py
@@ -135,9 +135,10 @@ async def gemini_session_handler(client_websocket: websockets.WebSocketServerPro
 
 async def main() -> None:
     # async with websockets.serve(gemini_session_handler, "localhost", 9080):
-    port = int(os.environ.get("PORT", 8000))
+    # port = int(os.environ.get("PORT", 8000))
     # port = os.getenv("PORT", 8000)
-    print("portt:", port)
+    port = 8000
+    print("portttt:", port)
     async with websockets.serve(gemini_session_handler, "0.0.0.0", port):
         print(f"Running websocket server on 0.0.0.0:{port}...")
         await asyncio.Future()  # Keep the server runnin<g indefinitely
@@ -148,7 +149,8 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    # port = int(os.environ.get("PORT", 8000))
     # port = os.getenv("PORT", 8000)
-    print("porttt:", port)
+    port = 8000
+    print("portttttt:", port)
     uvicorn.run(app, host="0.0.0.0", port=port)
